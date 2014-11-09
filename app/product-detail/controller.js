30878fd4673ba981677f75e220078e8e0760de63
tanoy kumar <tanoy@neevtech.com>
2014-11-09 16:51:37 +0530

    UI changes in pdp page

--- app/product-detail/controller.js
+++ app/product-detail/controller.js
@@ -11,13 +11,14 @@ angular.module('myApp.product-detail', ['ngRoute'])
 
 .controller('ProductDetailCtrl', ['$scope', '$rootScope', 'xhrFactory', function($scope, $rootScope, xhrFactory) {
 	$scope.selectedUrl = "images/pdp/big_bag.jpg";
+	$scope.toggleOpen = true;
 	jQuery(document).foundation({
 	  orbit: {
 	   animation: 'slide',
      animation_speed: 500,
      navigation_arrows: false,
      bullets: true,
-     timer: true,
+     timer: false,
      timer_speed: 800,
      pause_on_hover: true, 
     resume_on_mouseout: true,
@@ -28,4 +29,14 @@ angular.module('myApp.product-detail', ['ngRoute'])
 		$scope.selectedUrl = url;
 	}
   $rootScope.layoutClass="";
+  $scope.toggleCustom = function(){
+  	if($scope.toggleOpen){
+  	 jQuery(".heightAccordian").css("height", "0");
+  	 jQuery(".heightAccordian").css("padding-top", "0");
+  	}else{
+  		jQuery(".heightAccordian").css("height", "7rem");
+  	 	jQuery(".heightAccordian").css("padding-top", "1rem");
+  	}
+  	$scope.toggleOpen = $scope.toggleOpen === false ? true: false;
+  }
 }]);
