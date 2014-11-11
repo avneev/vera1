6bdff87cb580d0aa60867552c2ef7e362db07002
Anubhav Vishwakarma <anubhav@neevtech.com>
2014-11-11 17:48:22 +0530

    tuesday update

--- header/controller.js
+++ header/controller.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp.header', [])
 .controller('HeaderCtrl',  ['$scope', '$rootScope', 'xhrFactory', function($scope, $rootScope, xhrFactory) {
-	
+	$scope.showSearchPanel = false;
 	xhrFactory.getList('nav-left.json').then(
 		function(response) {
 			$scope.navleft = response;
@@ -20,5 +20,32 @@ angular.module('myApp.header', [])
 		item.subMenuVisible = false;
 		$scope.fixedSubmenu = false;
 	}
+	$scope.search = function(val){
+		
+		if(val.length >= 2 && val === 'we'){
+			$scope.showSearchPanel = true;
+		}else {
+			$scope.showSearchPanel = false;
+		}
+	};
+
+	function closeSearch () {
+		$('.searchbox').removeClass('searchbox-open');
+		$('.searchbox-input').focusout();
+	 	$('.sticky-nav').removeClass('fixed-header-search-expanded');
+	   	$('.right-nav').show(25);
+	   	$('.left-nav').show(25);
+	   	$('.img-margin').show(25);
+	   	$('.search-logo').hide();
+	   	$('#close-search').hide();
+	    $('.search').removeClass('search-expanded');
+	    $('.img-margin').removeClass('img-expanded');
+	 }
+
+	$scope.hideSearch = function(val) {
+		closeSearch();	
+		val = "";
+		$scope.showSearchPanel = false;
+	};
 
 }]);
---
+++ search/controller.js
@@ -0,0 +1,123 @@
+'use strict';
+
+angular.module('myApp.search', [])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/search', {
+    templateUrl: 'search/view.html',
+    controller: 'SearchCtrl'
+  });
+}])
+
+.controller('SearchCtrl',  ['$scope', '$rootScope', 'xhrFactory', function($scope, $rootScope, xhrFactory) {
+	$rootScope.layoutClass="Search";
+	$scope.showQuickView = false;
+	$scope.filtersList = [];
+	$rootScope.breadCrumb = "<a href='#'>HOME</a> / <a style='font-weight:bold;' href='#/product-listing'>BAGS</a>";
+	xhrFactory.getList('quick-view.json').then(
+		function(response) {
+			$scope.quickViewListing = response;		},
+		function(error) {
+			console.log(error);
+		}
+	)
+
+	xhrFactory.getList('quick-view-filter.json').then(
+		function(response) {
+			$scope.quickViewFliterListing = response;	
+		},
+		function(error) {
+			console.log(error);
+		}
+	)
+
+	xhrFactory.getList('product-listing1.json').then(
+		function(response) {
+			$scope.plpListing = response;		
+		},
+		function(error) {
+			console.log(error);
+		}
+	)
+
+	xhrFactory.getList('filter-listing.json').then(
+		function(response) {
+			$scope.filterListing = response;
+		},
+		function(error) {
+			console.log(error);
+		}
+	)
+
+	xhrFactory.getList('filter-categories.json').then(
+		function(response) {
+			$scope.categories = response;
+			$scope.selectedCategory = $scope.categories[0].imgList;
+			$scope.categories[0].active = true;
+		},
+		function(error) {
+			console.log(error);
+		}
+	)
+
+	$scope.changeCategory = function(item) {
+		angular.forEach($scope.categories, function(value, key){
+			value.active = false;
+		});
+		$scope.selectedCategory = item.imgList;
+		item.active = true;
+	};
+
+	$scope.addFilter = function(item) {
+		item.active = true;
+		$scope.filtersList.push(item)
+	};
+
+	$scope.removeFilter = function(item) {
+		item.active = false;
+		angular.forEach($scope.filtersList, function(value, key){
+			if(value.name == item.name)
+				$scope.filtersList.splice(key,1); 
+		});
+	};
+
+	$scope.quickView = function(product, ele) {
+		$scope.showQuickView = true;
+		$scope.productView = product;
+		setTimeout(function(){
+			jQuery('html, body').animate({
+		        scrollTop: jQuery("."+ele).offset().top-200
+		    }, 1500);
+		},100);
+	};
+	$scope.closeQuickView = function() {
+		$scope.showQuickView = false;
+		$scope.productView = [];
+		setTimeout(function(){
+			jQuery('html, body').animate({
+		        scrollTop: jQuery(".quick-view-row").offset().top-200
+		    }, 1500);
+		},100);
+	};
+
+	xhrFactory.getList('product-listing-search.json').then(
+		function(response) {
+			$scope.productListing = response;
+		},
+		function(error) {
+			console.log(error);
+		}
+	)
+
+	$(document).foundation({
+	  orbit: {
+	    animation: 'slide',
+	    timer_speed: 5000,
+	    pause_on_hover: true,
+	    animation_speed: 1000,
+	    navigation_arrows: false,
+	    bullets: true,
+	    timer:false
+	  }
+	});
+}]);
